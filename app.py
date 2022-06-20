93da811372b2686cd8a3b07cf9fed726ca4a62e8
Steven Lance <44912347+stevenjlance@users.noreply.github.com>
2022-06-20 20:27:51 +0000

    Code Along

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
-
-
+from flask import render_template
+from flask import request
+from model import get_breakfast_rating
+from datetime import datetime
 # -- Initialization section --
 app = Flask(__name__)
 
@@ -13,4 +13,23 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+	props = {
+		"title": "My Breakfast Page",
+		"first_name": "Steven",
+	}
+	return render_template("index.html", time = datetime.now(), props = props)
+
+@app.route("/results", methods = ["GET", "POST"])
+def results():
+	if request.method == "POST":
+		print(request.form["breakfast"])
+		user_name = request.form["nickname"]
+		user_breakfast = request.form["breakfast"]
+		breakfast_rating = get_breakfast_rating(user_breakfast)
+		return render_template("breakfast.html", user_name = user_name, user_breakfast = user_breakfast, breakfast_rating = breakfast_rating)
+	else:
+		return "ERROR"
+
+@app.route("/secret")
+def secret():
+	return "<h3>You found the secret page</h3>"
